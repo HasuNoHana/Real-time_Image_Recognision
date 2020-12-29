5f245c6ee77fda2f7fc85108ace59845e48a3def
Turowska <alicjalom@wp.pl>
2020-12-29 20:37:17 +0100

    utworzenie odbioru ramki z kamery i wyswietlenie wybranych

--- A.cpp
+++ A.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
 
 int main() {
-    std::cout << "hello world" << std::endl;
+    	cv::VideoCapture camera(0);
+	if(!camera.isOpened()){
+		std::cout<<"Nie otworzono kamere\n";
+		return 1;
+	}
+	cv::namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+	cv::Mat frame;
+	int i = 0;
+	while(1){
+		camera >> frame;
+		if(cv::waitKey(10) >= 0){
+			cv::imshow("Webcam", frame);
+			++i;
+			if(i==6){
+				break;
+			}
+		}
+	}
+	return 0;
 }
