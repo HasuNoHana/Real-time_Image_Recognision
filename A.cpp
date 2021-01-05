ebff186cee290d29564aae37f0b90ccbd6fc35fb
Turowska <alicjalom@wp.pl>
2021-01-05 18:24:26 +0100

    Proba wstawiania do pamieci wspoldzielonej

--- A.cpp
+++ A.cpp
@@ -5,6 +5,7 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <cstdio>
+#include <cstring>
 
 
 #define KLUCZ_KOLEJKA 12345
@@ -22,6 +23,7 @@ struct bufmsg{
 int main() {
 
 	cv::Mat frame;
+	cv::Mat* shared_frame;
 	bufmsg buf;
 
 	//otwarcie kolejki
@@ -38,12 +40,21 @@ int main() {
 		return 1;
 	}
 
+	//otwarcie bufora pamięci współdzielonej
+	shared_frame = (cv::Mat*) shmat(id_pamieci, 0, 0);
+	if(shared_frame==(cv::Mat*)-1){
+		std::cout<<"Blad otwarcia bufora pamieci\n";
+		return 1;
+	}
+
 	//utworzenie deskryptora kamery
     	cv::VideoCapture camera(0);
 	if(!camera.isOpened()){
 		std::cout<<"Nie otworzono kamere - blad\n";
-		msgctl(id_kolejki, IPC_RMID, NULL);
-		shmctl(id_pamieci, IPC_RMID, NULL);
+		//zamkniecie bufora pamieci
+		if(shmdt(shared_frame)==-1){
+			std::cout<<"Blad zamkniecia bufora pamieci\n";
+		}
 		return 1;
 	}
 
@@ -55,15 +66,20 @@ int main() {
 
 		//czy inny proces oczekuje na klatkę
 		if(msgrcv(id_kolejki, &buf, ROZMIAR_KOMUNIKATU, 2, IPC_NOWAIT)!=-1){
-			//tu będzie wstawienie ramki do pamięci współdzielonej (otworzenie pamięci - shmat, modyfikacja pamięci, zamknięcie pamięci - shmdt)
-
-
+			//wstawienie ramki do pamięci współdzielonej
+			memcpy(shared_frame, &frame, sizeof(frame));
 	
 			//wysylanie komunikatu
 			buf.mtype = 1;
 			sprintf(buf.mtext, "1");
 			if(msgsnd(id_kolejki, &buf, ROZMIAR_KOMUNIKATU, 0)==-1){
 				std::cout<<"Nie wyslano komunikatu - blad\n";
+				//zamkniecie bufora pamieci
+				if(shmdt(shared_frame)==-1){
+					std::cout<<"Blad zamkniecia bufora pamieci\n";
+				}
+				//zamkniecie kamery
+				camera.release();
 				return 1;
 			}
 
@@ -76,5 +92,13 @@ int main() {
 			}//do usunięcia
 		}
 	}
+
+	//zamkniecie bufora pamieci
+	if(shmdt(shared_frame)==-1){
+		std::cout<<"Blad zamkniecia bufora pamieci\n";
+	}
+	//zamkniecie kamery
+	camera.release();
+
 	return 0;
 }
